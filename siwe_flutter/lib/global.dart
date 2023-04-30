caa6b6e25c4bb20f8db5dec56c612f138cdec04c
zerotimedrift <hevar@metrorun.com>
2023-04-30 12:10:46 +0100

    Update global.dart

--- siwe_flutter/lib/global.dart
+++ siwe_flutter/lib/global.dart
@@ -1,7 +1,9 @@
 import 'package:walletconnect_dart/walletconnect_dart.dart';
 import 'package:web3dart/web3dart.dart';
+import 'package:walletconnect_qrcode_modal_dart/walletconnect_qrcode_modal_dart.dart';
 
 EthereumWalletConnectProvider? ethereumProvider;
+WalletConnectQrCodeModal? walletModal;
 String? message;
 String? signature;
 String? address;
