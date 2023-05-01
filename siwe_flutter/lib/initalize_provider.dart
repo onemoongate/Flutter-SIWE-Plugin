315ed1d1cd42f57f9cb64f5bb9d049fa30f2d259
zerotimedrift <hevar@metrorun.com>
2023-05-01 21:10:04 +0100

    Make initialize provider await signature

--- siwe_flutter/lib/initalize_provider.dart
+++ siwe_flutter/lib/initalize_provider.dart
@@ -3,10 +3,34 @@ import 'package:walletconnect_qrcode_modal_dart/walletconnect_qrcode_modal_dart.
 import 'package:walletconnect_dart/walletconnect_dart.dart';
 import 'package:web3dart/web3dart.dart';
 import 'global.dart';
+import 'dart:async';
+
+class WalletConnectObserver extends WidgetsBindingObserver {
+  final VoidCallback onAppResumed;
+
+  WalletConnectObserver({required this.onAppResumed});
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.resumed) {
+      WidgetsBinding.instance?.addPostFrameCallback((_) {
+        onAppResumed();
+      });
+    }
+  }
+}
 
 /// Initalize a provider, you must supply: context to display modal, bridge url, app name, app description, your app url, and icon to display on connection.
 Future<void> initializeProvider(BuildContext context, String bridge,
     String name, String description, String url, String icon) async {
+  // Create a Completer for waiting until the app is resumed
+  Completer<void> appResumedCompleter = Completer();
+
+  // Create a WalletConnectObserver and add it to the WidgetsBinding
+  WalletConnectObserver observer = WalletConnectObserver(onAppResumed: () {
+    appResumedCompleter.complete();
+  });
+  WidgetsBinding.instance?.addObserver(observer);
   // Create a connector
   walletModal = WalletConnectQrCodeModal(
     connector: WalletConnect(
@@ -30,10 +54,12 @@ Future<void> initializeProvider(BuildContext context, String bridge,
     print('Error: $error');
     return null;
   });
+  await appResumedCompleter.future;
 
   // Create a provider and set it to the global variable
   ethereumProvider = EthereumWalletConnectProvider(walletModal!.connector);
   final sender = EthereumAddress.fromHex(session!.accounts[0]);
   // set address to global variable
   address = sender.hexEip55;
+  WidgetsBinding.instance?.removeObserver(observer);
 }
