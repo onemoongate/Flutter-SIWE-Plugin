fafdfc2a7d248cce2061220cbf413b2178451066
ZeroTimeDrift <403.hevar@gmail.com>
2023-05-03 14:09:06 +0100

    Update siwe.dart

--- lib/siwe.dart
+++ lib/siwe.dart
@@ -5,8 +5,8 @@ import 'create_message.dart';
 import 'sign_message.dart';
 
 // Sign message
-Future<void> signInWithEthereum(
-    domain, statement, uri, version, chainId) async {
+Future<void> signInWithEthereum(String domain, String statement, String uri,
+    String version, String chainId) async {
   // Create message to sign with nonce
   final message = createMessage(domain, statement, uri, version, chainId);
   // Sign message
